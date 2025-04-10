c5adae63d60988440f61379b3de7147d42dcbe53
Vincent Guillet <vincent.guillet@outlook.com>
2025-04-10 11:11:54 +0200

    create menu model

--- src/fr/groupe5/restaurant/menu/Menu.java
+++ src/fr/groupe5/restaurant/menu/Menu.java
@@ -1,4 +1,38 @@
 package fr.groupe5.restaurant.menu;
 
 public class Menu {
+
+    private int idMenu;
+    private String nomMenu;
+    private int prixMenu;
+
+    public Menu(int idMenu, String nomMenu, int prixMenu) {
+        this.idMenu = idMenu;
+        this.nomMenu = nomMenu;
+        this.prixMenu = prixMenu;
+    }
+
+    public int getIdMenu() {
+        return idMenu;
+    }
+
+    public void setIdMenu(int idMenu) {
+        this.idMenu = idMenu;
+    }
+
+    public String getNomMenu() {
+        return nomMenu;
+    }
+
+    public void setNomMenu(String nomMenu) {
+        this.nomMenu = nomMenu;
+    }
+
+    public int getPrixMenu() {
+        return prixMenu;
+    }
+
+    public void setPrixMenu(int prixMenu) {
+        this.prixMenu = prixMenu;
+    }
 }
