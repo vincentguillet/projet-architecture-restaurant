b03b57b35988d675cebbe09c11e233ea4036b2b7
Vincent Guillet <vincent.guillet@outlook.com>
2025-04-10 11:29:17 +0200

    create reservation model

--- src/fr/groupe5/restaurant/reservation/Reservation.java
+++ src/fr/groupe5/restaurant/reservation/Reservation.java
@@ -1,4 +1,40 @@
 package fr.groupe5.restaurant.reservation;
 
+import java.time.LocalDateTime;
+
 public class Reservation {
+
+    private int idReservation;
+    private short nombrePersonnes;
+    private LocalDateTime dateReservation;
+
+    public Reservation(int idReservation, short nombrePersonnes, LocalDateTime dateReservation) {
+        this.idReservation = idReservation;
+        this.nombrePersonnes = nombrePersonnes;
+        this.dateReservation = dateReservation;
+    }
+
+    public int getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(int idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public short getNombrePersonnes() {
+        return nombrePersonnes;
+    }
+
+    public void setNombrePersonnes(short nombrePersonnes) {
+        this.nombrePersonnes = nombrePersonnes;
+    }
+
+    public LocalDateTime getDateReservation() {
+        return dateReservation;
+    }
+
+    public void setDateReservation(LocalDateTime dateReservation) {
+        this.dateReservation = dateReservation;
+    }
 }
