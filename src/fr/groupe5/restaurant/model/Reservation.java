be92b38317fa756f3bad02b254fb2f91767387be
Vincent Guillet <vincent.guillet@outlook.com>
2025-04-10 12:04:48 +0200

    add client parameter to reservation model

--- src/fr/groupe5/restaurant/model/Reservation.java
+++ src/fr/groupe5/restaurant/model/Reservation.java
@@ -7,11 +7,13 @@ public class Reservation {
     private int idReservation;
     private short nombrePersonnes;
     private LocalDateTime dateReservation;
+    private Client client;
 
-    public Reservation(int idReservation, short nombrePersonnes, LocalDateTime dateReservation) {
+    public Reservation(int idReservation, short nombrePersonnes, LocalDateTime dateReservation, Client client) {
         this.idReservation = idReservation;
         this.nombrePersonnes = nombrePersonnes;
         this.dateReservation = dateReservation;
+        this.client = client;
     }
 
     public int getIdReservation() {
@@ -26,6 +28,10 @@ public class Reservation {
         return nombrePersonnes;
     }
 
+    public Client getClient() {
+        return client;
+    }
+
     public void setNombrePersonnes(short nombrePersonnes) {
         this.nombrePersonnes = nombrePersonnes;
     }
@@ -37,4 +43,8 @@ public class Reservation {
     public void setDateReservation(LocalDateTime dateReservation) {
         this.dateReservation = dateReservation;
     }
-}
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+ }
