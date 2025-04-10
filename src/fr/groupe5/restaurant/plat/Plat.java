205ea78d7631be476d5c61989bf8725ac82ba341
Virginie <143293036+Virginouille@users.noreply.github.com>
2025-04-10 11:26:32 +0200

    Creation class, objet, getter, setter, package plat

--- src/fr/groupe5/restaurant/plat/Plat.java
+++ src/fr/groupe5/restaurant/plat/Plat.java
@@ -1,4 +1,34 @@
 package fr.groupe5.restaurant.plat;
 
 public class Plat {
+    private String nomPlat;
+    private String descriptionPlat;
+    private int prixPlat;
+
+public Plat(String nomPlat, String descriptionPlat, int prixPlat) {
+    this.nomPlat = nomPlat;
+    this.descriptionPlat = descriptionPlat;
+    this.prixPlat = prixPlat;
+    }
+
+//Getter
+    public String getNomPlat() {
+        return this.nomPlat;
+    }
+    public String getDescriptionPlat() {
+    return this.descriptionPlat;
+    }
+    public int getPrixPlat() {
+    return this.prixPlat;
+    }
+//Setter
+    public void setNomPlat(String nomPlat) {
+    this.nomPlat = nomPlat;
+    }
+    public void setDescriptionPlat(String descriptionPlat) {
+    this.descriptionPlat = descriptionPlat;
+    }
+    public void setPrixPlat(int prixPlat) {
+    this.prixPlat = prixPlat;
+    }
 }
